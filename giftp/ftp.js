94db0506b8e96c554c65e23e87c4033d0166720f
Sema <sema@pyva.net>
2016-02-06 00:26:19 -0500

    Working version

---
+++ giftp/ftp.js
@@ -0,0 +1,86 @@
+var Ftp = require('ftp');
+var fs = require('fs');
+
+function connect(arg, func) {
+  if (arg.sftp) throw "sftp is not yet implemented";
+  var addr = arg.ftp.split(':');
+  var host = addr[0];
+  var port = addr[1];
+  var conn = {host:host};
+  if (arg.login) conn.user = arg.login;
+  if (arg.password) conn.password = arg.password;
+  var ftp = new Ftp();
+  ftp.on('ready', function(){func(ftp);});
+  ftp.connect(conn);
+}
+
+function getIfPossible(ftp, remote, local, func) {
+  ftp.get(remote, function(err, stream){
+    if (err) {
+      //console.log(err);
+      func(ftp);
+      return;
+    }
+    stream.once('close', function(){func(ftp);});
+    stream.pipe(fs.createWriteStream(local));
+  });
+}
+
+function mkdir(ftp, remote, arr, func) {
+  var count = arr.length;
+  if (!count) {
+    func(ftp);
+    return;
+  }
+  function callback(err) {
+    if (err) {
+      console.log(err);
+    }
+    process.stdout.write('.');
+    count--;
+    if (!count) func(ftp);
+  }
+  for (var i in arr) ftp.mkdir(remote+'/'+arr[i], true, callback);
+}
+
+function send(ftp, remote, local, arr, func) {
+  var count = arr.length;
+  if (!count) {
+    func(ftp);
+    return;
+  }
+  function callback(err) {
+    if (err) {
+      console.log(err);
+    }
+    process.stdout.write('.');
+    count--;
+    if (!count) func(ftp);
+  }
+  for (var i in arr) ftp.put(local+'/'+arr[i], remote+'/'+arr[i], callback);
+}
+
+function remove(ftp, remote, arr, func) {
+  var count = arr.length;
+  if (!count) {
+    func(ftp);
+    return;
+  }
+  function callback(err) {
+    if (err) {
+      console.log(err);
+    }
+    process.stdout.write('.');
+    count--;
+    if (!count) func(ftp);
+  }
+  for (var i in arr) ftp.delete(remote+'/'+arr[i], callback);
+}
+
+module.exports = {
+  connect: connect,
+  send: send,
+  remove: remove,
+  mkdir: mkdir,
+  getIfPossible: getIfPossible
+};
--- giftp/index.js
+++ giftp/index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var git = require('./git.js');
+var ftp = require('./ftp.js');
 
 console.log(git.version());
 
@@ -10,7 +11,7 @@ try {
   config = process.argv[3];
 }
 catch (e) {
-  contents = fs.readFileSync('settings.json');
+  contents = fs.readFileSync('giftp.json');
   config = process.argv[2];
 }
 var conf = JSON.parse(contents);
@@ -20,21 +21,90 @@ if (config !== undefined) {
     throw 'Invalid config name: ' + config;
   }
 }
-var local = conf['local'];
-var remote = conf['remote'];
-var ftp = conf['ftp'];
-var sftp = conf['sftp'];
 
-if (local === undefined) throw 'Local path undefined';
-local = fs.realpathSync(local);
-if (remote === undefined) throw 'Remote path undefined';
-if (ftp === undefined && ftp === undefined) throw 'Neither ftp nor sftp is defined';
-if (!fs.lstatSync(local).isDirectory() || git.root(local) === undefined) throw local + ' is not a valid GIT directory';
+if (conf.local === undefined) throw 'Local path undefined';
+conf.local = fs.realpathSync(conf.local);
+if (conf.remote === undefined) throw 'Remote path undefined';
+if (conf.ftp === undefined && conf.sftp === undefined) throw 'Neither ftp nor sftp is defined';
+if (!fs.lstatSync(conf.local).isDirectory() || git.root(conf.local) === undefined) throw conf.local + ' is not a valid GIT directory';
 
-console.log('head =', git.head(local), '!');
-
-var diff = git.changes(local);
+var diff = git.changes(conf.local);
 if (diff['M'] || diff['A'] || diff['D']) throw 'Please commit the changes!';
 
+var localRev = conf.local+'/.giftp';
+var remoteRev = conf.remote+'/.giftp';
+var oldRev = git.null;
+var newRev = git.head(conf.local);
 
-console.log('#2', git.changes(local, git.null));
+ftp.connect(conf, getLastSha);
+
+function getLastSha(conn) {
+  ftp.getIfPossible(conn, remoteRev, localRev, updateFiles);
+}
+
+function updateFiles(conn) {
+  try {
+    oldRev = '' + fs.readFileSync(localRev);
+  }
+  catch (e) {}
+  console.log('old rev:', oldRev);
+  console.log('new rev:', newRev);
+  console.log('');
+  var diff = git.changes(conf.local, oldRev);
+  for (var k in diff.A) console.log('A\t' + diff.A[k]);
+  for (var k in diff.M) console.log('M\t' + diff.M[k]);
+  for (var k in diff.D) console.log('D\t' + diff.D[k]);
+  if (!diff.A && !diff.D && !diff.M) {
+    console.log('No changes!');
+    done(conn);
+    return;
+  }
+  var mkDir = {};
+  var rmDir = {};
+  pushSubpaths(diff.A, mkDir);
+  pushSubpaths(diff.M, mkDir);
+  pushSubpaths(diff.D, rmDir);
+  var dd = [''];
+  for (var d in mkDir) dd.push(d);
+  dd = dd.sort();
+  var mkdir = [];
+  for (var i=0; i<dd.length; i++) if (dd[i] && (i == dd.length-1 || dd[i+1].substr(0, dd[i].length) != dd[i])) mkdir.push(dd[i]);
+  var send = [];
+  if (diff.A) send = send.concat(diff.A);
+  if (diff.M) send = send.concat(diff.M);
+  var remove = [];
+  if (diff.D) remove = remove.concat(diff.D);
+  ftp.remove(conn, conf.remote, remove, function(){
+    ftp.mkdir(conn, conf.remote, mkdir, function(){
+      ftp.send(conn, conf.remote, conf.local, send, updateRevision);
+    });
+  });
+}
+
+function updateRevision(conn) {
+  fs.writeFileSync(localRev, newRev);
+  ftp.send(conn, conf.remote, conf.local, ['.giftp'], done);
+}
+
+function done(conn) {
+  conn.end();
+  console.log('\nDone!');
+}
+
+
+function dir(path) {
+  var a = path.split('/');
+  if (a.length) a[a.length-1] = '';
+  return a.join('/');
+}
+
+function pushSubpaths(a, h) {
+  for (var i in a) {
+    var p = a[i].split('/');
+    if (p.length < 2) continue;
+    for (var n = p.length - 1; n > 0; n--) {
+      p.length = n; p.push('');
+      h[p.join('/')] = true;
+    }
+  }
+}
